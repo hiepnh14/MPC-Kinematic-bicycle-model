1d8d79b98fb4da5d48d041149a26f9e4fdccd0e4
hiepnguyen <hiepnguyen2000pro@gmail.com>
2022-06-17 17:04:01 +0900

    Kinematic Bicycle model upload

---
+++ Test1/template_model.py
@@ -0,0 +1,74 @@
+
+import numpy as np
+from casadi import *
+from casadi.tools import *
+import pdb
+# from math.tools import *
+import sys
+sys.path.append('../')
+import do_mpc
+def template_model(symvar_type='SX'):
+    """
+    --------------------------------------------------------------------------
+    template_model: Variables / RHS / AUX
+    --------------------------------------------------------------------------
+    """
+
+    # Obtain an instance of the do-mpc model class
+    # and select time discretization:
+    model_type = 'discrete' # either 'discrete' or 'continuous'
+    model = do_mpc.model.Model(model_type, symvar_type)
+    dt = 0.05 #time step
+    # Introduce new states, inputs and other variables to the model, e.g.:
+    _x = model.set_variable(var_type='_x', var_name='x', shape=(6,1))
+    # [x, y, theta, v, a, delta]
+    _u = model.set_variable(var_type='_u', var_name='u', shape=(2,1))
+    # [a, delta]
+    #Set expression
+    model.set_expression(expr_name='cost', expr=sum1(((_x[1]-2)**2 + (_x[3]- 0.1)**2)))
+    # Set right-hand-side of ODE for all introduced states (_x).
+    # Names are inherited from the state definition.
+    A = np.array([[ 1, 0, 0,  dt*cos(_x[2]), 0, 0],
+                  [ 0, 1, 0,  dt*sin(_x[2]), 0, 0],
+                  [ 0, 0, 1,  dt*tan(_u[1])/3.0, 0, 0],
+                  [ 0, 0, 0, 1, dt, 0],
+                  [ 0, 0, 0, 0, 0, 0],
+                  [ 0, 0, 0, 0, 0, 0]])
+    # A = np.array([[1, 0, 0, dt],
+    #               [0, 1, 0, dt],
+    #               [0, 0, 1, 0],
+    #               [0, 0, 0, 1]])
+    B = np.array([[0, 0],
+                  [0, 0],
+                  [0, 0],
+                  [0, 0],
+                  [1, 0],
+                  [0, 1]])
+    # # Setup model:
+    # A = np.array([[0.763, 0.460, 0.115, 0.020],
+    #               [-0.899, 0.763, 0.420, 0.115],
+    #               [0.115, 0.020, 0.763, 0.460],
+    #               [0.420, 0.115, -0.899, 0.763]])
+    #
+    # B = np.array([[0.014, 0.1],
+    #               [0.063, 0.021],
+    #               [0.221, 0.012],
+    #               [0.367, 0.324]])
+
+    x_next = A@_x+B@_u
+    # x_next = model.set_variable(var_type='_z', var_name='x_next', shape=(4,1))
+    # x_next[0, 0] = _x[0, 0] + _x[3, 0] * cos(_x[2, 0]) * dt
+    # x_next[1, 0] = _x[1, 0] + _x[3, 0] * sin(_x[2, 0]) * dt
+    # x_next[2, 0] = _x[2, 0] + _x[3, 0] * tan(_u[1, 0])/3.0 * dt
+    # x_next[3, 0] = _x[3, 0] + _u[0, 0] * dt
+    model.set_rhs('x', x_next)
+    model.setup()
+
+    # x_next = A @ _x + dt * np.array([_u[0, 0] * cos(_x[2, 0])], [_u[0, 0] * sin(_x[2, 0])], [_u[0, 0] * tan(_u[1, 0]) / 1])
+    # model.set_rhs('x', x_next)
+    # model.set_alg('x_next', x_next - A @ _x - B @ _u)
+    # model.set_alg('x_next', x_next-A@_x-np.array([_u[0,0]*cos(_x[2,0])], [_u[0,0]*sin(_x[2,0])], [_u[0,0]*tan(_u[1,0])/1]))
+    # x_next = A @ _x + B @ _u
+
+
+    return model
