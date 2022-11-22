423e5dc96e8111d253ea5ddeaaa3c713572d7a04
hiepnguyen <hiepnguyen2000pro@gmail.com>
2022-11-22 18:43:50 +0900

    Bug fixed

--- KBM/template_simulator.py
+++ KBM/template_simulator.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-from casadi import *
-from casadi.tools import *
-import pdb
-import sys
-sys.path.append('../../')
-import do_mpc
-
-def template_simulator(model):
-    # Obtain an instance of the do-mpc simulator class
-    # and initiate it with the model:
-    simulator = do_mpc.simulator.Simulator(model)
-
-    # Set parameter(s):
-    simulator.set_param(t_step = 0.01)
-
-    # Optional: Set function for parameters and time-varying parameters.
-
-    # Setup simulator:
-    simulator.setup()
-
-=======
 import numpy as np
 from casadi import *
 from casadi.tools import *
@@ -42,5 +19,4 @@ def template_simulator(model):
     # Setup simulator:
     simulator.setup()
 
->>>>>>> 2a19200d1d826db7ef0dbd185f888e3c94ea8957
     return simulator
